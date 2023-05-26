c083e396902a41201c2cfe17c2b97a85e1e26253
dk <omnipresent9@yandex.ru>
2023-05-26 14:23:19 +0300

    Убрал комменты

--- app/src/main/java/ru/dk/mydictionary/App.kt
+++ app/src/main/java/ru/dk/mydictionary/App.kt
@@ -6,21 +6,6 @@ import ru.dk.mydictionary.di.DaggerAppComponent
 
 class App : Application() {
 
-//    private val retrofit = Retrofit.Builder()
-//        .baseUrl("https://dictionary.skyeng.ru/")
-//        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-//        .addConverterFactory(GsonConverterFactory.create())
-//        .build()
-
-//    private val api: SearchListApi by lazy { retrofit.create(SearchListApi::class.java) }
-//    private val ioScheduler: Scheduler by lazy { Schedulers.io() }
-//    private val uiScheduler: Scheduler by lazy { AndroidSchedulers.mainThread() }
-//    private val repository: SearchListRepo by lazy { SearchListRepoImpl(api, ioScheduler) }
-
-//    val viewModelFactory: ViewModelFactory by lazy {
-//        ViewModelFactory(repository, uiScheduler)
-//    }
-
     val appComponent: AppComponent by lazy { DaggerAppComponent.create() }
 
     override fun onCreate() {
